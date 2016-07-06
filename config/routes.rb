432cfc7a5530d43978911101f894e5bd7c05690e
Tony Bagalini <tony.bagalini@gmail.com>
2016-07-06 15:20:18 -0700

    added custom devise fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :inventories
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root "pages#home"
   get "about" => "pages#about"
   get "search" => "pages#search"
