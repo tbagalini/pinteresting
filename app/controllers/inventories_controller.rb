88155a780f415cb49c95f81271de916e90069413
Tony Bagalini <tony.bagalini@gmail.com>
2016-07-06 11:00:00 -0700

    added inventory scaffold

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,73 +1,43 @@
 class InventoriesController < ApplicationController
   before_action :set_inventory, only: [:show, :edit, :update, :destroy]
-
-  # GET /inventories
-  # GET /inventories.json
   def index
     @inventories = Inventory.all
   end
-
-  # GET /inventories/1
-  # GET /inventories/1.json
   def show
   end
 
-  # GET /inventories/new
   def new
     @inventory = Inventory.new
   end
 
-  # GET /inventories/1/edit
   def edit
   end
-
-  # POST /inventories
-  # POST /inventories.json
   def create
     @inventory = Inventory.new(inventory_params)
 
-    respond_to do |format|
-      if @inventory.save
-        format.html { redirect_to @inventory, notice: 'Inventory was successfully created.' }
-        format.json { render :show, status: :created, location: @inventory }
-      else
-        format.html { render :new }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
-      end
+    if @inventory.save
+      redirect_to @inventory, notice: 'Inventory was successfully created.'
+    else
+      render :new
     end
   end
-
-  # PATCH/PUT /inventories/1
-  # PATCH/PUT /inventories/1.json
   def update
-    respond_to do |format|
-      if @inventory.update(inventory_params)
-        format.html { redirect_to @inventory, notice: 'Inventory was successfully updated.' }
-        format.json { render :show, status: :ok, location: @inventory }
-      else
-        format.html { render :edit }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
-      end
+    if @inventory.update(inventory_params)
+      redirect_to @inventory, notice: 'Inventory was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /inventories/1
-  # DELETE /inventories/1.json
   def destroy
     @inventory.destroy
-    respond_to do |format|
-      format.html { redirect_to inventories_url, notice: 'Inventory was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to inventories_url, notice: 'Inventory was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_inventory
       @inventory = Inventory.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def inventory_params
       params.require(:inventory).permit(:description, :part_number, :alternate_part_number, :condition_code, :qty, :mfg_code, :serial_number, :part_comments, :price)
     end
--- app/views/inventories/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@inventories) do |inventory|
-  json.extract! inventory, :id, :description, :part_number, :alternate_part_number, :condition_code, :qty, :mfg_code, :serial_number, :part_comments, :price
-  json.url inventory_url(inventory, format: :json)
-end
