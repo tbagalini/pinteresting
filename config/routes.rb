6f28ade0b0708ea6697d063f23ee0952d989e736
Tony Bagalini <tony.bagalini@gmail.com>
2016-07-06 19:11:18 -0700

    fixed form footers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :inventories
   devise_for :users, :controllers => { registrations: 'registrations' }
+
   root "pages#home"
   get "about" => "pages#about"
   get "search" => "pages#search"
-
+  get "import" => "pages#import"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
