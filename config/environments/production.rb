1fc569b3fe1d845b9efec599bed8451fc1981438
Tony Bagalini <tony.bagalini@gmail.com>
2016-07-07 18:15:10 -0700

    added cloudfront cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.action_controller.asset_host = d13w5ndaruk327.cloudfront.net
+  config.action_controller.asset_host = "d13w5ndaruk327.cloudfront.net"
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
